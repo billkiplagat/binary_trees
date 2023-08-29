6467160367f7e2ed3c413f4ba5f584d3e397c062
billkiplagat <bill.kiplagat.7@gmail.com>
2023-08-29 12:56:59 +0300

    Updated task 10 condition

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
 * binary_tree_depth - calls recursive_depth to return the depth
 * of a node in a binary tree
@@ -6,17 +7,22 @@
 * @tree: tree
 * Return: depth of the tree or 0 if tree is NULL;
 */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-if (tree == NULL)
-{
-return (0);
+size_t binary_tree_depth(const binary_tree_t *tree) {
+    if (tree == NULL) {
+        return (0);
+    }
+    return (recursive_depth(tree));
 }
-size_t depth = 0;
-while (tree->parent != NULL)
+/**
+ * recursive_depth - measures the depth of a node in a binary tree
+ *
+ * @tree: tree
+ * Return: depth of a node in a binary tree
+ */
+
+size_t recursive_depth(const binary_tree_t *tree)
 {
-tree = tree->parent; /*Move up the tree towards the root*/
-depth++;
-}
-return (depth);
+    if (tree == NULL)
+        return (-1);
+    return (recursive_depth(tree->parent) + 1);
 }
