3fb66374fdd17513d97523fb57d0b3d9c2846fd3
billkiplagat <bill.kiplagat.7@gmail.com>
2023-08-29 13:14:14 +0300

    Updated task 11

--- binary_trees.h
+++ binary_trees.h
@@ -41,4 +41,5 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t recursive_depth(const binary_tree_t *tree);
 size_t recursive_height(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
