9070eb11b02aebd4c36e0e6d8a6a12d7b4b3aea9
billkiplagat <bill.kiplagat.7@gmail.com>
2023-08-29 14:17:56 +0300

    Updated task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,10 +7,12 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-if (tree == NULL) {
+if (tree == NULL)
+{
 return (0);
 }
-if (tree->left == NULL && tree->right == NULL) {
+if (tree->left == NULL && tree->right == NULL)
+{
 return 1;
 }
 /* Recursively count the leaves in the left and right subtrees
