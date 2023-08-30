9e924bfcc76cf7a0557fd8076c05bc2e4e74be52
billkiplagat <bill.kiplagat.7@gmail.com>
2023-08-30 10:57:34 +0300

    Updated task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,6 +9,21 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
-size_t has_child = (tree->left != NULL || tree->right != NULL) ? 1 : 0;
-return has_child + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+if (binary_tree_is_parent(tree))
+return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+return (0);
+}
+
+/**
+* binary_tree_is_parent - checks if a node is a parent
+*
+* @node: pointer to the node
+* Return: 1 if node is a parent, otherwise 0
+*/
+int binary_tree_is_parent(const binary_tree_t *node)
+{
+int parent = 0;
+if (node && (node->left || node->right))
+parent = 1;
+return (parent);
 }
